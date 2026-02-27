672eb467b69774b4d57edc143a67ac25536dc847
Chris Materick <cjmaterick@gmail.com>
2026-02-26 22:23:55 -0600

    Added basic input validation when submitting ID
    Remember to add more specific validation if invalid inputs cause slowdown on backend.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,16 +10,43 @@ export default function Home() {
     // Shows on page now for debugging purposes, REMOVE LATER
     const [payload, setPayload] = useState(null);
 
+    const [error, setError] = useState(null);
+
+    // Checks if the input in field is a valid Steam64 ID or vanity URL, (TODO LATER: add more validation if sending potential invalid input slows backend code)
+    const isValidSteamId = (steamId) => {
+        const id = steamId.trim();
+
+        // Check if Steam64ID (17 digit int)
+        if (/^\d{17}$/.test(id)) return true;
+
+        // Check if vanity URL (3-32 alphanumeric chars)
+        //if (/^[a-zA-Z0-9]{3,32}$/.test(id)) return true;
+
+        return false;
+    }
+
     // Runs when form is submitted
     // Prepares the payload object for backend
     const handleSubmit = (e) => {
         // Prevent page from refreshing
         e.preventDefault();
 
+        // Trim whitespace from input
+        const trimmedId = steamId.trim();
+
+        // Validate the input before preping payload
+        if (!isValidSteamId(trimmedId)) {
+            setError("Please enter a valid Steam64 ID.");
+            return;
+        }
+
+        // No active error
+        setError(null);
+
         // JSON object to send to backend
         // Trims whitespace in case of user error, (TODO: add validation for user input)
         const requestBody = {
-            steamId: steamId.trim()
+            steamId: trimmedId
         };
 
         // Save the payload to state so it can be displayed on page for debugging purposes, REMOVE LATER
@@ -50,6 +77,11 @@ export default function Home() {
                         placeholder="7656119XXXXXXXXXX"
                         style={{ display: "block", width: "100%", marginTop: "8px"}}
                     />
+
+                    {error && (
+                        <p style={{ color: "#ff6b6b", fontSize: "0.85rem" }}>{error}</p>
+                    )}
+
                     {/* Submit button (TODO: Figure out what we want it to say) */}
                     <button type="submit" style={{ marginTop: 16 }}>
                         Get Recommendations
