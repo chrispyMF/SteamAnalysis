d8031bc5b2a6959036d97eb842b9446915398aed
Chris Materick <cjmaterick@gmail.com>
2026-03-02 14:20:34 -0600

    Fancier cards implemented

--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -5,12 +5,14 @@ const teamMembers = [
         name: "Chris Materick",
         role: "Frontend Developer",
         github: "https://github.com/chrispyMF",
+        githubUsername: "chrispyMF",
         avatar: "https://avatars.githubusercontent.com/u/121414227?v=4"
     },
     {
         name: "Fair Day",
         role: "SUPER AWESOME ROLE",
-        github: "https://github.com/chrispyMF",
+        github: "https://github.com/Fair-Day-Labs",
+        githubUsername: "Fair;Day Labs",
         avatar: "https://avatars.githubusercontent.com/u/212604035?s=200&v=4"
     }
 ];
@@ -35,7 +37,15 @@ export default function Team() {
 
                         <p className="team-role">{member.role}</p>
 
-                        <a href={member.github} target="_blank" rel="noreferrer" className="team-github">GitHub Profile</a>
+                        <a href={member.github} target="_blank" rel="noreferrer" className="team-github">
+                            <img src = "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub Logo" className="team-github-logo"/>
+                            <span>{member.githubUsername}</span>
+                        </a>
+
+                        <div className="team-spacer"/>
+
+                        <div className="team-socials">{/*Add social buttons later*/}</div>
+                        
                     </div>
                 ))}
             </div>
